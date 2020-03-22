373cfd876605f262c9c8cda1256923fc760a33f5
miguel141097 <j2mc1410@gmail.com>
2020-03-22 18:45:28 +0000

    Comentarios de cabecera

--- src/add.js
+++ src/add.js
@@ -1,4 +1,18 @@
 /**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Asignatura: Programación de Aplicaciones Interactivas
+ * Curso: 3º
+ *
+ * @author Miguel Ordoñez
+ * @author Basilio Gómez
+ * @since 23 de Marzo del 2020
+ * @desc  Contiene la implemetanción de la función suma.
+ * @link https://github.com/ULL-ESIT-INF-PAI-2019-2020/2019-2020-pai-trabajo-testing-javascript_testing-_miguel_y_basilio
+ */
+
+/**
  * @desc Dado dos números los suma
  * @param {number} valueOne primer parámetro de la suma
  * @param {number} valueTwo segundo parámetro de la suma
